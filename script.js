0604385e7127be69d8e91e7a80452c7ec7dd4877
Oni030 <51912581+Oni030@users.noreply.github.com>
2024-06-21 17:29:41 +0200

    Wrap the function in a eventListener for DOMContentLoaded.

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const container = document.querySelector('.container');
 
-function createDivs(amount) {
-    for (let i = 0; i < Math.pow(amount, 2); i++) {
-        const newDiv = document.createElement('div');
-        container.appendChild(newDiv);
+document.addEventListener('DOMContentLoaded', function() {
+    function createDivs(amount) {
+        for (let i = 0; i < Math.pow(amount, 2); i++) {
+            const newDiv = document.createElement('div');
+            container.appendChild(newDiv);
+        };
     };
-};
+});
 
 createDivs(4);
