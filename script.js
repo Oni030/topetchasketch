a62b279a0103d38a3e7e39dbce70365a729faa1a
Oni030 <51912581+Oni030@users.noreply.github.com>
2024-06-21 17:34:39 +0200

    Revert "Wrap the function in a eventListener for DOMContentLoaded."
    This reverts commit 0604385e7127be69d8e91e7a80452c7ec7dd4877.

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 const container = document.querySelector('.container');
 
-document.addEventListener('DOMContentLoaded', function() {
-    function createDivs(amount) {
-        for (let i = 0; i < Math.pow(amount, 2); i++) {
-            const newDiv = document.createElement('div');
-            container.appendChild(newDiv);
-        };
+function createDivs(amount) {
+    for (let i = 0; i < Math.pow(amount, 2); i++) {
+        const newDiv = document.createElement('div');
+        container.appendChild(newDiv);
     };
-});
+};
 
 createDivs(4);
