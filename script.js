951ce1d9ab2db22f93fc3f6c901745ef4f87e44e
Oni030 <51912581+Oni030@users.noreply.github.com>
2024-06-21 17:44:24 +0200

    Create blockSize().
    Calculates width/height of each block.

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
     const container = document.querySelector('.container');
 
+    function blockSize(amount) {
+        return (100/amount) + '%';
+    };
+
     function createDivs(amount) {
         for (let i = 0; i < Math.pow(amount, 2); i++) {
             const newDiv = document.createElement('div');
             newDiv.classList.add('block');
+            newDiv.style.width = `${blockSize(amount)}`;
+            newDiv.style.height = `${blockSize(amount)}`;
             container.appendChild(newDiv);
         };
     };
