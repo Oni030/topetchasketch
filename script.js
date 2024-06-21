02c5075948699d37e477a2a3b8479bf59d90ad7e
Oni030 <51912581+Oni030@users.noreply.github.com>
2024-06-21 17:40:32 +0200

    Create css-class .block, set margin, padding and box-sizing. Add .block class to every div element created by createDivs().

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
     function createDivs(amount) {
         for (let i = 0; i < Math.pow(amount, 2); i++) {
             const newDiv = document.createElement('div');
+            newDiv.classList.add('block');
             container.appendChild(newDiv);
         };
     };
