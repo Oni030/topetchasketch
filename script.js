36f9b4a48685a7b9da5951dfcf6728028c4ff503
Oni030 <51912581+Oni030@users.noreply.github.com>
2024-06-21 17:25:10 +0200

    Create a function that generates "amount" numbers of div elements.

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 const container = document.querySelector('.container');
+
+function createDivs(amount) {
+    for (let i = 0; i < Math.pow(amount, 2); i++) {
+        const newDiv = document.createElement('div');
+        container.appendChild(newDiv);
+    };
+};
+
+createDivs(4);
