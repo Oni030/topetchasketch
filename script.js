b5359a85453a2b971ae82f0ca12c1ed49a7c4c59
Oni030 <51912581+Oni030@users.noreply.github.com>
2024-06-21 17:37:52 +0200

    Wrap everything in callback() for eventListener 'DOMContentLoaded'.
    This ensures that the js code is run after the DOM content is loaded and ready.

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-const container = document.querySelector('.container');
+document.addEventListener('DOMContentLoaded', function() {
+    const container = document.querySelector('.container');
 
-function createDivs(amount) {
-    for (let i = 0; i < Math.pow(amount, 2); i++) {
-        const newDiv = document.createElement('div');
-        container.appendChild(newDiv);
+    function createDivs(amount) {
+        for (let i = 0; i < Math.pow(amount, 2); i++) {
+            const newDiv = document.createElement('div');
+            container.appendChild(newDiv);
+        };
     };
-};
-
-createDivs(4);
+    
+    createDivs(4);
+})
