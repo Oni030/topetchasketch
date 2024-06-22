dc481b48b035876c69bdca3810db947dad7f52b1
Oni030 <51912581+Oni030@users.noreply.github.com>
2024-06-22 19:44:52 +0200

    Create and add setGrid() to .set-grid button.
    setGrid() opens a pop-up for the user to put in the desired number of blocks per side.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const container = document.querySelector('.container');
+    const setGridButton = document.querySelector('.set-grid');
 
     function blockSize(amount) {
         return (100/amount) + '%';
@@ -7,7 +8,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     function borderSize(amount) {
         return (1/Math.sqrt(amount)) + 'px solid lightgrey';
-    }
+    };
 
     function createDivs(amount) {
         for (let i = 0; i < Math.pow(amount, 2); i++) {
@@ -20,7 +21,29 @@ document.addEventListener('DOMContentLoaded', function() {
         };
     };
 
-    createDivs(100);
+    createDivs(4);
+
+
+    function setGrid() {
+        const userInput = prompt('Type in the desired number of blocks per side (max. 100)', 4);
+
+        if (userInput === null) {
+            alert('No Input. Please put in a number between 2 and 100!');
+        } else {
+            if(userInput > 100) {
+                alert('The input exceeds 100 blocks per side. Please choose a lower number!');
+            } else 
+            if (userInput <= 1) {
+                alert('The input is too low. Please choose an input higher than 1!');
+            } else {
+                createDivs(userInput);
+            };
+        };
+    };
+
+
+    setGridButton.addEventListener('click', setGrid);
+    
     
     let drawColor = 'black';
 
