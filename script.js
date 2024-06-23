4d9708b833245540150ebe880870224ef67e42be
Oni030 <51912581+Oni030@users.noreply.github.com>
2024-06-23 17:48:55 +0200

    .block borders removed.
    borders were removed for aesthetic reasons.

--- script.js
+++ script.js
@@ -7,17 +7,12 @@ document.addEventListener('DOMContentLoaded', function() {
         return (100/amount) + '%';
     };
 
-    function borderSize(amount) {
-        return (1/Math.sqrt(amount)) + 'px solid lightgrey';
-    };
-
     function createDivs(amount) {
         for (let i = 0; i < Math.pow(amount, 2); i++) {
             const newDiv = document.createElement('div');
             newDiv.classList.add('block');
             newDiv.style.width = `${blockSize(amount)}`;
             newDiv.style.height = `${blockSize(amount)}`;
-            newDiv.style.border = `${borderSize(amount)}`;
             container.appendChild(newDiv);
         };
     };
